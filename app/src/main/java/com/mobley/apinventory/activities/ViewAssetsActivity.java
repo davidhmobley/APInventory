efe7f0ab72cbeac51c048fb1bdfcb6493002760c
david mobley <davidhmobley@gmail.com>
2019-05-13 16:40:31 -0400

    start to add search to ViewAssetsActivity

--- app/src/main/java/com/mobley/apinventory/activities/ViewAssetsActivity.java
+++ app/src/main/java/com/mobley/apinventory/activities/ViewAssetsActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -99,17 +100,18 @@ public class ViewAssetsActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_view_assets, menu);
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_view_assets, menu);
 
         // Get the SearchView and set the searchable configuration
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+
         // Assumes current activity is the searchable activity
         //searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         //searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
         //searchView.setSubmitButtonEnabled(true);
-        //searchView.setIconifiedByDefault(false)
 
         return true;
     }
