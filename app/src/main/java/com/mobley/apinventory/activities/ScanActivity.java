19d425a82930df6fe09485d14b15d2f3b6572e31
david mobley <davidhmobley@gmail.com>
2019-05-14 15:54:34 -0400

    hide keyboard on "scan"

--- app/src/main/java/com/mobley/apinventory/activities/ScanActivity.java
+++ app/src/main/java/com/mobley/apinventory/activities/ScanActivity.java
@@ -1,5 +1,6 @@
 package com.mobley.apinventory.activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -95,6 +97,8 @@ public class ScanActivity extends AppCompatActivity implements View.OnClickListe
         boolean bGood = true;
 
         if (view == mGoButton) {
+            hideKeyboard(this);
+
             if (TextUtils.isEmpty(mScanAssetET.getText())) {
                 bGood = false;
                 mScanAssetTV.setTextColor(Color.RED);
@@ -117,5 +121,16 @@ public class ScanActivity extends AppCompatActivity implements View.OnClickListe
                 mSqlDataSource.close();
             }
         }
+    }
+
+    private void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
